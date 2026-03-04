aa7342cf306b392001eb4d02d686a977357d12e6
renaldy akbar <portorenal@gmail.com>
2026-03-05 01:40:19 +0700

    test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,16 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
 
-const app = new Hono()
+const app = new Hono();
 
-const welcomeStrings = [
-  'Hello Hono!',
-  'To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono'
-]
+const welcomeStrings = ["Hello Hono!", "Halo kakrey"];
 
-app.get('/', (c) => {
-  return c.text(welcomeStrings.join('\n\n'))
-})
+app.get("/", (c) => {
+  return c.text(welcomeStrings.join("\n\n"));
+});
 
-export default app
+serve(app, () => {
+  console.log("Server is running on http://localhost:3000");
+});
+
+export default app;
